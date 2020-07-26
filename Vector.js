82d20ba853e4cb9d692a3e81a7aceee4e4fcf891
Marcus Luebke <marcusxluebke@gmail.com>
2020-07-25 23:48:00 -0700

    Create Vector.js

---
+++ Vector.js
@@ -0,0 +1,29 @@
+class Vector {
+	constructor(x,y){
+		this.x = x;
+		this.y = y;
+	}
+	static isVector(v){
+		return v instanceof Vector;
+	}
+	add(x,y){
+		if(Vector.isVector(x)){
+			return new Vector(this.x + x.x, this.y + x.y);
+		} else {
+			return new Vector(this.x + x, this.y + y);
+		}
+	}
+	subtract(x,y){
+		if(Vector.isVector(x)){
+			return new Vector(this.x - x.x, this.y - x.y);
+		} else {
+			return new Vector(this.x - x, this.y - y);
+		}
+	}
+	scale(scalar){
+		return new Vector(scalar*this.x, scalar*this.y);
+	}
+	scaleXY(scalar_x, scalar_y){
+		return new Vector(scalar_x*this.x, scalar_y*this.y);
+	}
+}
